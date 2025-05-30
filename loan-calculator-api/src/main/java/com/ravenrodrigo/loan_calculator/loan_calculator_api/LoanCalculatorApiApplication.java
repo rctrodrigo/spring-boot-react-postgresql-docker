e5bdde71a07a9da9d4eea2455ad95eab49b08d78
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2025-05-30 20:51:04 +0800

    Add initial spring boot project
    This commit has the initial Spring Boot project generated by Spring
    Initializr web-based tool to have minimal Spring configuration. It
    simplifies the implementation of data access layers, and provides
    building RESTful APIs. The project has the dependencies of Spring Data
    JPA, Spring Web, Spring Boot DevTools, PostgreSQL Driver, and Lombok.
    The project uses Java version 17 and Spring Boot version 3.5.0.

---
+++ loan-calculator-api/src/main/java/com/ravenrodrigo/loan_calculator/loan_calculator_api/LoanCalculatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.ravenrodrigo.loan_calculator.loan_calculator_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanCalculatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanCalculatorApiApplication.class, args);
+	}
+
+}
---
+++ loan-calculator-api/src/test/java/com/ravenrodrigo/loan_calculator/loan_calculator_api/LoanCalculatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravenrodrigo.loan_calculator.loan_calculator_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanCalculatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
